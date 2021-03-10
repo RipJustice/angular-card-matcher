19839b2b8f8139e62799b0a4b820ffb0b317dfc5
smullen <mrhoipoi@gmail.com>
2021-03-09 20:10:22 -0500

    Tweaking styling

--- src/app/pages/card-match/card-match.component.spec.ts
+++ src/app/pages/card-match/card-match.component.spec.ts
@@ -1,5 +1,5 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CardMatchComponent } from './card-match.component';
 
 describe('CardMatchComponent', () => {
@@ -8,7 +8,8 @@ describe('CardMatchComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CardMatchComponent ]
+      declarations: [ CardMatchComponent ],
+      imports: [ HttpClientTestingModule ]
     })
     .compileComponents();
   });
